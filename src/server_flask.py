9ad49537e7da206b1739f9950cedd73911ffb158
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-09-04 16:47:14 -0300

    plotando grafico de bode Corretamente

--- src/server_flask.py
+++ src/server_flask.py
@@ -1,6 +1,10 @@
+import io
+import base64
 from flask import Flask, jsonify, render_template, request
 from scipy import signal
-import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import serial
 
 app = Flask(__name__)
@@ -29,31 +33,27 @@ def atualizar_potenciometro():
         try:
             linha = ser.readline().decode('utf-8').strip()
             valor_corrigido = round((3 * int(linha)) / 4095, 3)
-            # print(valor_corrigido)
-        except ValueError:
-            pass
-        
+        except ValueError as v:
+            print(f"Erro ao atualizar valor: {v}")
     return jsonify({'valor': valor_corrigido})
-
-# def ler_dados_serial():
     
 
 @app.route('/salvar-kp', methods=['POST'])
 def salvar_kp():
     global kp
-    kp = request.json.get('valor')
+    kp = float(request.json.get('kp'))
     return jsonify({'status': 'ok', 'kp': kp})
 
 @app.route('/salvar-kd', methods=['POST'])
 def salvar_kd():
-    global kd
-    kd = request.json.get('valor')
+    global kd 
+    kd = float(request.json.get('kd'))
     return jsonify({'status': 'ok', 'kd': kd})
 
 @app.route('/salvar-ki', methods=['POST'])
 def salvar_ki():
     global ki
-    ki = request.json.get('valor')
+    ki = float(request.json.get('ki'))
     return jsonify({'status': 'ok', 'ki': ki})
 
 @app.route('/salvar-valores', methods=['POST'])
@@ -61,82 +61,72 @@ def salvar_valores():
     dados = request.json
 
     try:
-        kp = float(dados.get('kp', 0))
-        kd = float(dados.get('kd', 0))
-        ki = float(dados.get('ki', 0))
+    
         tau = float(dados.get('tau', 0.01))  # Valor padrão
         qsi = float(dados.get('qsi', 0.5))  # Valor padrão
-        modo = dados.get('modo', 'continua')
-
-        # Verificar tempo de amostragem
-        if modo == 'digital':
-            try:
-                tempo = float(dados.get('tempo', 0.1))
-            except ValueError:
-                tempo = 0.1
-        else:
-            tempo = 0.01  # Tempo de amostragem padrão para contínuo
-
-        # Numerador e denominador para a função de transferência G(s)
+        modo = dados.get('modo', 'continuo')
+        tipo = dados.get("tipo", "PID")
         num = [kd, kp, ki]
         den = [1/(tau**2), 2*qsi/tau, 1]
 
-        if modo == 'digital':
-            # Discretizar o sistema para modo digital
-            sistema = signal.cont2discrete((num, den), tempo, method='zoh')
-            num_d, den_d = sistema[0], sistema[1]
-            sistema_discreto = signal.dlti(num_d, den_d)
-            
-            # Cálculo da resposta em frequência (Bode) para sistema digital
-            w, mag, fase = signal.dlti(num_d, den_d).bode()
-            
-            # Cálculo da resposta ao degrau
-            t, yout = signal.dstep(sistema_discreto)
-            t = np.array(t).flatten()
-            yout = np.array(yout).flatten()
-
-            # Cálculo do Lugar das Raízes (LDR) para sistema digital
-            k = np.linspace(0, 10, num=500)
-            rlist = []
-            for k_val in k:
-                num_k = [x * k_val for x in num_d]
-                sistema_k = signal.dlti(num_k, den_d)
-                poles = np.roots(sistema_k.den)
-                rlist.append(poles.real.tolist())  # Converta para lista
-
-            dados_bode = {'frequencia': w.tolist(), 'magnitude': mag.tolist(), 'fase': fase.tolist()}
-            dados_ldr = {'raizes': rlist, 'ganhos': k.tolist()}
+        if(modo == "digital"):
+            tempo = float(dados.get("tempo"))
+            bode_image = transform_bode(numerador=num, denominador=den, tempo=tempo,modo=modo, tipo=tipo)
 
         else:
-            # Sistema contínuo
-            sistema = signal.TransferFunction(num, den)
-            
-            # Cálculo da resposta em frequência (Bode)
-            w, mag, fase = signal.bode(sistema)
-            
-            # Cálculo da resposta ao degrau
-            t, yout = signal.step(sistema)
-
-            # Cálculo do Lugar das Raízes (LDR)
-            k = np.linspace(0, 10, num=100)
-            rlist = []
-            for k_val in k:
-                num_k = [x * k_val for x in num]
-                sistema_k = signal.TransferFunction(num_k, den)
-                poles = np.roots(sistema_k.den)
-                rlist.append(poles.real.tolist())  # Converta para lista
-
-            dados_bode = {'frequencia': w.tolist(), 'magnitude': mag.tolist(), 'fase': fase.tolist()}
-            dados_degrau = {'tempo': t.tolist(), 'resposta': yout.tolist()}
-            dados_ldr = {'raizes': rlist, 'ganhos': k.tolist()}
-
-        dados_degrau = {'tempo': t.tolist(), 'resposta': yout.tolist()}
-
-        return jsonify({'bode': dados_bode, 'degrau': dados_degrau, 'ldr': dados_ldr})
+            tempo = 0.1
+            bode_image = transform_bode(numerador=num, denominador=den, tempo=tempo,modo=modo, tipo=tipo)
+        
+        return jsonify({"bode_image": bode_image})
     
     except Exception as e:
         return jsonify({'erro': str(e)}), 500
 
+def transform_bode(numerador: float, denominador: float,  tempo: float, modo: str, tipo: str) -> str:
+
+    if(modo == "digital"):
+        sistema = signal.cont2discrete((numerador, denominador), tempo, method='zoh') # Faz a Função de Transferência
+        num_d, den_d = sistema[0], sistema[1];
+        
+        # Calculo da resposta em frequência
+        w, mag, fase = signal.dlti(num_d, den_d).bode()
+        
+    else:
+        sistema = signal.TransferFunction(numerador, denominador)
+
+        # Cálculo da resposta em frequência (Bode)
+        w, mag, fase = signal.bode(sistema)
+
+    return plote_bode(w, mag, fase,  tipo)
+
+def plote_bode(w, mag, fase, tipo):
+    # Cria o gráfico
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6))
+
+    # Plota gráfico de magnitude
+    ax[0].semilogx(w, mag)
+    ax[0].set_title(f'Gráfico de Bode - {tipo.upper()}')
+    ax[0].set_xlabel('Frequência [rad/s]')
+    ax[0].set_ylabel('Magnitude [dB]')
+
+    # Plota gráfico de fase
+    ax[1].semilogx(w, fase)
+    ax[1].set_xlabel('Frequência [rad/s]')
+    ax[1].set_ylabel('Fase [graus]')
+
+    plt.tight_layout()
+
+    # Salva o gráfico em um buffer
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0) # Posição contigua da memória
+
+    # Converte a imagem para Base64
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    buf.close()
+
+    # Retorna a string Base64
+    return image_base64
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)
--- src/static/control.js
+++ src/static/control.js
@@ -1,25 +1,26 @@
+function salvarValor(param) {
+    var valor = $(`#${param}`).val();
+    $.ajax({
+        url: `/salvar-${param}`,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ [param]: valor }),
+        success: function(response) {
+            alert(`${JSON.stringify(response, null, 2)} salvo com sucesso!`);
+        }
+    });
+}
+
 $(document).ready(function() {
     function ajustarVisibilidadeTempo() {
         var modo = $("#modo").val();
         $("#tempoDiv").toggle(modo === "digital");
     }
 
-    function salvarValor(param) {
-        var valor = $(`#${param}`).val();
-        $.ajax({
-            url: `/salvar-${param}`,
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ valor: valor }),
-            success: function(response) {
-                alert(`${param.toUpperCase()} salvo com sucesso!`);
-            }
-        });
-    }
-
     function enviarValores() {
         var modo = $("#modo").val();
         var tempo = modo === 'digital' ? $("#tempo").val() : 0.1;
+        var tipo = $("input[name='tipoControle']:checked").val() || "erro"
         var tau = $("#tau").val() || 0.01;
         var qsi = $("#qsi").val() || 0.5;
 
@@ -30,14 +31,16 @@ $(document).ready(function() {
             data: JSON.stringify({
                 modo: modo,
                 tempo: tempo,
+                tipo: tipo,
                 tau: tau,
                 qsi: qsi,
-                kp: $("#kp").val(),
-                kd: $("#kd").val(),
-                ki: $("#ki").val()
             }),
             success: function(response) {
-                plotarGraficos(response);
+                
+                if(response.bode_image){
+                    var imgSrc = 'data:image/png;base64,' + response.bode_image;
+                    $('#bodeImage').attr('src', imgSrc);
+                }
             }
         });
     }
@@ -54,38 +57,7 @@ $(document).ready(function() {
         });
     }
 
-    function plotarGraficos(dados) {
-        var trace1 = {
-            x: dados.bode.frequencia,
-            y: dados.bode.magnitude,
-            mode: 'lines',
-            name: 'Bode - Magnitude'
-        };
-        var trace2 = {
-            x: dados.bode.frequencia,
-            y: dados.bode.fase,
-            mode: 'lines',
-            name: 'Bode - Fase'
-        };
-        var trace3 = {
-            x: dados.degrau.tempo,
-            y: dados.degrau.resposta,
-            mode: 'lines',
-            name: 'Resposta ao Degrau'
-        };
-        var trace4 = {
-            x: dados.ldr.ganhos,
-            y: dados.ldr.raizes,
-            mode: 'lines',
-            name: 'Lugar das Raízes'
-        };
-
-        Plotly.newPlot('graficoBode', [trace1, trace2]);
-        Plotly.newPlot('graficoDegrau', [trace3]);
-        Plotly.newPlot('graficoLDR', [trace4]);
-    }
-
-    setInterval(atualizarPotenciometro, 1000);
+    setInterval(atualizarPotenciometro, 500);
     $("#modo").change(ajustarVisibilidadeTempo);
     $("#enviar").click(enviarValores);
 });
