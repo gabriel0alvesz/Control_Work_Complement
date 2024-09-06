3eecce2b896a07e7fce4e02bb1acaad17589a668
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-09-05 21:20:01 -0300

    o que deu pra fazer com o tempo, possui erros

--- src/server_flask.py
+++ src/server_flask.py
@@ -1,26 +1,26 @@
 import io
 import base64
-from flask import Flask, jsonify, render_template, request
-from scipy import signal
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import control as ctrl
 import serial
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-# Variáveis para armazenar os valores dos inputs
+# valores dos inputs
 kp = 0
 kd = 0
 ki = 0
 tau = 0.0
-qsi = 0.0
 tempo = 0.0
 valor_corrigido = 0.0
 modo_operacao = "continua"
+epsilon = 1e-10
 
-# Inicializa a conexão serial
-ser = serial.Serial('/dev/tty.usbserial-0001', 115200)  # Atualize para a porta correta
+ser = serial.Serial('/dev/tty.usbserial-0001', 115200)
+
 
 @app.route('/')
 def index():
@@ -32,7 +32,7 @@ def atualizar_potenciometro():
     if ser.in_waiting > 0:
         try:
             linha = ser.readline().decode('utf-8').strip()
-            valor_corrigido = round((3 * int(linha)) / 4095, 3)
+            valor_corrigido = round((10 * int(linha)) / 4095, 3)
         except ValueError as v:
             print(f"Erro ao atualizar valor: {v}")
     return jsonify({'valor': valor_corrigido})
@@ -61,71 +61,100 @@ def salvar_valores():
     dados = request.json
 
     try:
-    
-        tau = float(dados.get('tau', 0.01))  # Valor padrão
-        qsi = float(dados.get('qsi', 0.5))  # Valor padrão
+        tau = float(dados.get('tau', 0.01))  
         modo = dados.get('modo', 'continuo')
         tipo = dados.get("tipo", "PID")
-        num = [kd, kp, ki]
-        den = [1/(tau**2), 2*qsi/tau, 1]
+        tempo = float(dados.get("tempo"))
+        
+        # Função de transferência contínua G(s) = (Kp + Ki/s + Kd*s) / (1 + tau*s)
+
+        if(tipo == "PID"):
+            num = [kp, ki, kd]
+        elif(tipo == "PD"):
+            num = [kp, 0, kd]
+        elif(tipo == "PI"):
+            num = [kp, ki, 0]
+
+        den = [epsilon, tau, 1]
+
+        sistema_geral = ctrl.TransferFunction(num, den) # Cria o sistema padrão
 
         if(modo == "digital"):
-            tempo = float(dados.get("tempo"))
-            bode_image = transform_bode(numerador=num, denominador=den, tempo=tempo,modo=modo, tipo=tipo)
-
+            sistema_discreto = ctrl.sample_system(sistema_geral, tempo, method="zoh")
+           
+            bode_image = transform_bode(sistema=sistema_discreto, modo=modo, tipo=tipo)
+            degrau_image = transform_degrau(sistema=sistema_discreto, modo=modo, tipo=tipo)
+            lgr_image = transform_lgr(sistema=sistema_discreto, modo=modo, tipo=tipo)
         else:
-            tempo = 0.1
-            bode_image = transform_bode(numerador=num, denominador=den, tempo=tempo,modo=modo, tipo=tipo)
+            bode_image = transform_bode(sistema=sistema_geral, modo=modo, tipo=tipo)
+            degrau_image = transform_degrau(sistema=sistema_geral, modo=modo, tipo=tipo)
+            lgr_image = transform_lgr(sistema=sistema_geral, modo=modo, tipo=tipo)
         
-        return jsonify({"bode_image": bode_image})
+        return jsonify({
+            "tau": tau, 
+            "modo": modo,
+            "tempo": tempo, 
+            "tipo": tipo,
+            "Ks": [kp, ki, kd], 
+            "bode_image": bode_image,
+            "degrau_image": degrau_image,
+            "lgr_image": lgr_image
+        })
     
     except Exception as e:
+        print(str(e))
         return jsonify({'erro': str(e)}), 500
 
-def transform_bode(numerador: float, denominador: float,  tempo: float, modo: str, tipo: str) -> str:
+def transform_bode(sistema, modo: str, tipo: str) -> str:
+     
+    plt.figure()
+    ctrl.bode(sistema, dB=True)
+    plt.suptitle(f"Diagrama de Bode - Modo: {modo.upper()} - {tipo}", fontsize=12)
 
-    if(modo == "digital"):
-        sistema = signal.cont2discrete((numerador, denominador), tempo, method='zoh') # Faz a Função de Transferência
-        num_d, den_d = sistema[0], sistema[1];
-        
-        # Calculo da resposta em frequência
-        w, mag, fase = signal.dlti(num_d, den_d).bode()
-        
-    else:
-        sistema = signal.TransferFunction(numerador, denominador)
-
-        # Cálculo da resposta em frequência (Bode)
-        w, mag, fase = signal.bode(sistema)
-
-    return plote_bode(w, mag, fase,  tipo)
-
-def plote_bode(w, mag, fase, tipo):
-    # Cria o gráfico
-    fig, ax = plt.subplots(2, 1, figsize=(8, 6))
-
-    # Plota gráfico de magnitude
-    ax[0].semilogx(w, mag)
-    ax[0].set_title(f'Gráfico de Bode - {tipo.upper()}')
-    ax[0].set_xlabel('Frequência [rad/s]')
-    ax[0].set_ylabel('Magnitude [dB]')
-
-    # Plota gráfico de fase
-    ax[1].semilogx(w, fase)
-    ax[1].set_xlabel('Frequência [rad/s]')
-    ax[1].set_ylabel('Fase [graus]')
-
-    plt.tight_layout()
-
-    # Salva o gráfico em um buffer
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
-    buf.seek(0) # Posição contigua da memória
+    buf.seek(0)
 
-    # Converte a imagem para Base64
     image_base64 = base64.b64encode(buf.read()).decode('utf-8')
     buf.close()
 
-    # Retorna a string Base64
+    return image_base64
+
+def transform_degrau(sistema, modo: str, tipo: str) -> str:
+    
+    t, y = ctrl.step_response(sistema)
+    
+    plt.figure()
+    plt.plot(t, y)
+    plt.title(f"Resposta ao Degrau - Modo: {modo.upper()} - {tipo}", fontsize=12)
+    plt.xlabel('Tempo (s)')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+    
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    buf.close()
+
+    return image_base64
+
+def transform_lgr(sistema, modo: str, tipo: str) -> str:
+    
+    plt.figure()
+
+    ctrl.root_locus(sistema, plot=True)
+
+    plt.title(f"Lugar das Raízes (LGR) - Modo: {modo.upper()} - {tipo}", fontsize=12)
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    buf.close()
+
     return image_base64
 
 if __name__ == '__main__':
--- src/static/control.js
+++ src/static/control.js
@@ -6,7 +6,7 @@ function salvarValor(param) {
         contentType: 'application/json',
         data: JSON.stringify({ [param]: valor }),
         success: function(response) {
-            alert(`${JSON.stringify(response, null, 2)} salvo com sucesso!`);
+            $(`#${param}-btn`).addClass('saved');
         }
     });
 }
@@ -20,9 +20,8 @@ $(document).ready(function() {
     function enviarValores() {
         var modo = $("#modo").val();
         var tempo = modo === 'digital' ? $("#tempo").val() : 0.1;
-        var tipo = $("input[name='tipoControle']:checked").val() || "erro"
+        var tipo = $("input[name='tipoControle']:checked").val() || "PID"
         var tau = $("#tau").val() || 0.01;
-        var qsi = $("#qsi").val() || 0.5;
 
         $.ajax({
             url: '/salvar-valores',
@@ -33,16 +32,30 @@ $(document).ready(function() {
                 tempo: tempo,
                 tipo: tipo,
                 tau: tau,
-                qsi: qsi,
             }),
             success: function(response) {
+
+                exibeValores(response)
+
+                // console.log(response)
                 
                 if(response.bode_image){
-                    var imgSrc = 'data:image/png;base64,' + response.bode_image;
-                    $('#bodeImage').attr('src', imgSrc);
+                    var img_bode = "data:image/png;base64," + response.bode_image;
+                    var img_degrau = "data:image/png;base64," + response.degrau_image;
+                    var img_lgr = "data:image/png;base64," + response.lgr_image;
+                    $('#bodeImage').attr('src', img_bode);
+                    $('#degrauImage').attr('src', img_degrau);
+                    $('#lgrImage').attr('src', img_lgr);
                 }
             }
         });
+    }
+
+    function exibeValores(response) {
+        var texto = `Kp = ${response.Ks[0] || 0} \t Ki = ${response.Ks[1] || 0} \t Kd = ${response.Ks[2] || 0}\n
+        Modo = ${(response.modo)} \t Tempo = ${String(response.tempo)} \t Controle = ${String(response.tipo)}`;
+
+        $('#exibeValoresFinais span').html(texto)
     }
 
     function atualizarPotenciometro() {
@@ -56,6 +69,37 @@ $(document).ready(function() {
             }
         });
     }
+
+    // Função para resetar o formulário
+    function resetarFormulario() {
+        
+        document.getElementById('kp-btn').classList.remove('saved')
+        document.getElementById('ki-btn').classList.remove('saved')
+        document.getElementById('kd-btn').classList.remove('saved')
+
+        // Limpa a seleção de radio buttons
+        document.querySelectorAll('input[type="radio"]').forEach(radio => {
+            radio.checked = false;
+        });
+
+        // Limpa a seleção do dropdown
+        document.getElementById('modo').value = 'continuo';
+
+        // Oculta o tempoDiv
+        document.getElementById('tempoDiv').style.display = 'none';
+
+        // Limpa os valores finais exibidos
+        document.getElementById('exibeValoresFinais').innerHTML = '';
+
+        // Oculta as imagens
+        document.querySelectorAll('#graficoBode, #graficoDegrau, #graficoLDR').forEach(div => {
+            div.style.display = 'none';
+        });
+    }
+
+    // Adicione o event listener ao botão de reset
+    document.getElementById('reset').addEventListener('click', resetarFormulario);
+
 
     setInterval(atualizarPotenciometro, 500);
     $("#modo").change(ajustarVisibilidadeTempo);
